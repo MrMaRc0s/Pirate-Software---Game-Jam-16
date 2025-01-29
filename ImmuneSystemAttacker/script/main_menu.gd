ab66db16542cb55fed642ca78c9ac469e7af1a7e
kpetrout <kostas.petroutsos@gmail.com>
2025-01-30 00:13:39 +0200

    made some changes to the menus, fixed the scaling issues, TODO: add wiki and option settings

--- ImmuneSystemAttacker/script/main_menu.gd
+++ ImmuneSystemAttacker/script/main_menu.gd
@@ -2,17 +2,37 @@ class_name MainMenu
 extends Control
 
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton as Button
-@onready var quit_button =  $MarginContainer/HBoxContainer/VBoxContainer/QuitButton as Button
+@onready var wiki_button =  $MarginContainer/HBoxContainer/VBoxContainer/WikiButton as Button
+@onready var options_button = $MarginContainer/HBoxContainer/VBoxContainer/OptionsButton as Button
+@onready var options_menu = $Options_Menu as OptionsMenu
+@onready var margin_container = $MarginContainer as MarginContainer
 @onready var start_level = preload("res://Scenes/world.tscn") as PackedScene
 
 func _ready():
-	start_button.button_down.connect(on_start_pressed)
-	quit_button.button_down.connect(on_quit_down)
+	handle_connecting_signals()
 
 
 func on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
 
 
-func on_quit_down() -> void:
-	get_tree().quit()
+func on_options_pressed() -> void:
+	margin_container.visible = false
+	options_menu.set_process(true)
+	options_menu.visible = true
+
+
+func on_wiki_pressed() -> void:
+	pass
+
+
+func on_exit_options_menu() -> void:
+	margin_container.visible = true
+	options_menu.visible = false
+
+
+func handle_connecting_signals() -> void:
+	start_button.button_down.connect(on_start_pressed)
+	options_button.button_down.connect(on_options_pressed)
+	wiki_button.button_down.connect(on_wiki_pressed)
+	options_menu.exit_options_menu.connect(on_exit_options_menu)
