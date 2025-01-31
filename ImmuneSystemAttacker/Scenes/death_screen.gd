0662609347f843612d0d0b71d46730da575e326d
Galdas16 <chrisgaldan@gmail.com>
2025-01-31 10:35:46 +0200

    Tilemap

---
+++ ImmuneSystemAttacker/Scenes/death_screen.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _ready():
+	play()  # Force the video to start
+# Called when the node enters the scene tree for the first time.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):  # "Space" or "Enter" by default
+		skip_cinematic()
+
+func _on_finished():
+	skip_cinematic()
+
+func skip_cinematic():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
